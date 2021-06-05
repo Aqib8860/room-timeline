09caa0fdde6c70db9c5299cc8113020dc57a7e75
mrinmay678 <mrinmay678@gmail.com>
2021-06-06 00:04:42 +0530

    like comment

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,11 @@
 from starlette.routing import Route
 from .views import page_not_found
 from videos.urls import videos_urlpatterns
+from like_comment import lc_urlpatterns
 
 urlpatterns=[
     Route('/', page_not_found, methods=["GET","POST"]),
 ]
 
 urlpatterns.extend(videos_urlpatterns)
+urlpatterns.extend(lc_urlpatterns)
