958d136ccbce0d463d13594b94f859dca3484820
mrinmay678 <mrinmay678@gmail.com>
2021-06-18 19:45:13 +0530

    m

--- videos/models.py
+++ videos/models.py
@@ -1,14 +1,20 @@
 from server.settings import clientOpen
-
+from bson.json_util import loads, dumps
 class BaseUpload:
 
     def __init__(self):
         self.client = clientOpen()
     
     def create(self, profile_id ,title, description, duration, category, thumbnail, upload_file, timestamp):
+        profile=loads(dumps(self.client.videos.upload.find_one({"_id":profile_id})))
         self.client.videos.upload.insert_one({
             "_id":profile_id+"_"+title+"_"+str(timestamp)+"_"+category+"_"+str(duration),
-            "profile_id":profile_id,
+            "profile":{
+                "_id":profile_id,
+                "name":profile["name"],
+                "channel_name":profile["channel_name"],
+                "profile_picture":profile["profile_picture"]
+            },
             "title":title,
             "description":description,
             "thumbnail":thumbnail,
