7e1bbae573a577febee372453d56157a034f5365
mrinmay678 <mrinmay678@gmail.com>
2021-06-18 13:19:28 +0530

    make it better

---
+++ server/local_settings.py
@@ -0,0 +1,18 @@
+'''
+MONGODB CREDS
+'''
+username = "myworld"
+password = "hgrhtAAeybVuuHCc"
+
+'''
+AWS SERVER CREDS
+'''
+aws_access_key = 'AKIAUHZ3VLC5ZPVEAOXI'
+aws_secret_access_key = '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'
+aws_storage_bucket = 'myworld2021'
+aws_default_acl = 'public-read'
+
+'''
+AUTH  CREDS
+'''
+JWT_SECRET_KEY = 'i#%%(nqkzw#3xrkwj8f_#+yl4wz91_=#=u-8j1v-pbho_(r(u7'
